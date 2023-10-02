f56a9bf03242584440c3195b23aebde30fe0af2d
Luciano Pueyo <l.pueyo@bue.edu.ar>
2023-10-02 14:40:54 -0300

    actualizo el script para pngs con transparencia - ahora toma la carpeta originals y replica carpetas y subcarpetas en proccessed

--- scripts/replace_bg.py
+++ scripts/replace_bg.py
@@ -1,31 +1,49 @@
 from pathlib import Path
 from PIL import Image
 
+
+alpha_colors = (
+    (0,255,255,255), # Cyan
+    (255,0,110,255), # Magenta
+)
+
 original_folder = 'originals'
 processed_folder = 'processed'
 
 Path('originals').mkdir(parents=True, exist_ok=True)
 Path('processed').mkdir(parents=True, exist_ok=True)
 
-original_files = Path('originals').glob('*.png')
+# original_files = Path('originals').glob('*.png')
+original_files = Path('originals').glob('**/*')
 
 for original_file in original_files:
-    print(f'processing file: {original_file}')
-    
-    img = Image.open(original_file)
-    rgba = img.convert('RGBA')
-    datas = rgba.getdata()
+    inner_folders = f"{original_file.parent}/{original_file.name}"
+    if inner_folders.startswith(original_folder):
+        inner_folders = inner_folders.replace(original_folder, '')
+        
+    elif inner_folders.startswith(f"{original_folder}/"):
+        inner_folders = original_file.parent.replace(f"{original_folder}/", '')
 
 
-    newData = []
+    if original_file.is_dir():       
+        Path(f"{processed_folder}/{inner_folders}/").mkdir(parents=True, exist_ok=True)
+        
+    else:
+        print(f'processing file: {original_file}')
+        
+        img = Image.open(original_file)
+        rgba = img.convert('RGBA')
+        datas = rgba.getdata()
 
-    for item in datas:
-        if item == (0,255,255,255):
-            newData.append((255, 255, 255, 0))
 
-        else:
-            newData.append(item)
-            
-            
-    rgba.putdata(newData)
-    rgba.save(f'{processed_folder}/{original_file.name}', 'PNG')
+        newData = []
+        
+        for item in datas:
+            if item in alpha_colors:
+                newData.append((255, 255, 255, 0))
+
+            else:
+                newData.append(item)
+                              
+        rgba.putdata(newData)
+        rgba.save(f'{processed_folder}/{inner_folders}', 'PNG')
