c1f3ab7a534298cd27c6a36dd0488387efaedc38
Luciano Mauro Pueyo <luciano.pueyo@globant.com>
2023-10-31 18:01:41 -0300

    Map Editor generals

--- assets/Maps/Map Editor/Floor Tiles.tsx
+++ assets/Maps/Map Editor/Floor Tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="Floor Tiles" tilewidth="64" tileheight="64" spacing="5" margin="5" tilecount="147" columns="7">
- <image source="../../scripts/maps/PC Computer - DOOM 2 - Floor Textures.png" width="488" height="1454"/>
+ <image source="PC Computer - DOOM 2 - Floor Textures.png" width="488" height="1454"/>
 </tileset>
---
+++ assets/Maps/Map Editor/Items Tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Items_Tiles" tilewidth="64" tileheight="64" tilecount="36" columns="6">
+ <image source="Items_Tiles.png" trans="00ffff" width="384" height="384"/>
+</tileset>
